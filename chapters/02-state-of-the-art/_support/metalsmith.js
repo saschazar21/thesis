8ec4037552f5c9b22b8780d4def1e6821846f20b
Sascha Zarhuber <sascha.zarhuber@students.fh-hagenberg.at>
2017-04-06 20:19:40 +0200

    feat(chapter2): Added metalsmith

---
+++ chapters/02-state-of-the-art/_support/metalsmith.js
@@ -0,0 +1,24 @@
+const Metalsmith = require('metalsmith');
+const layouts = require('metalsmith-layouts');
+const markdown = require('metalsmith-markdown');
+
+Metalsmith(__dirname)
+.metadata({                  // define globally available variables
+  name: 'Test site',
+  url: 'http://localhost:3000'
+})
+.source('_src')              // the content directory
+.destination('_site')        // the output directory for compiled content
+.clean(true)                 // delete output directory first? yes!
+// (...)                     // Additional configuration
+.use(layouts({
+  engine: 'handlebars',
+  pattern: '**/*.html'       // Plugin would look for .html files, although
+}))                          // not yet processed by markdown renderer
+.use(markdown())
+.build((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Success!');
+})
